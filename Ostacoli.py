ccb8aa7fa9c5a3ae8863a454c80a9e3803c37135
moio97 <97moio97@gmail.com>
2016-05-11 10:04:27 +0200

    Ostacoli
    Funzione che disegna gli ostacoli dati.

---
+++ Ostacoli.py
@@ -0,0 +1,4 @@
+def ostacoli():
+    pygame.draw.polygon(screen,(0,255,255),[[550,300],[550,350],[500,325]],2)
+    pygame.draw.polygon(screen,(0,255,255),[[200,500],[200,550],[150,525]],2)
+    pygame.draw.rect(screen,(255,232,76),(0,800,1000,200),2)
